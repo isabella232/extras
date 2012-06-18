23d769fe7f343bd007fd4bc98149b07fe41aea5a
Greg Priday <greg@siteorigin.com>
2012-06-18 12:29:25 +0000

    Changes first run message to use iframes

--- admin/messages/message-firstrun.php
+++ admin/messages/message-firstrun.php
@@ -1,23 +1,13 @@
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=222225781217824";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 <?php _e("Hi! I'm Greg from SiteOrigin.", 'siteorigin') ?> 
 <?php _e("I hope you enjoy my free WordPress theme.", 'siteorigin') ?> 
 <?php _e("Please follow me on Twitter and Facebook for updates.", 'siteorigin') ?>
 
 <div class="social">
 	<div class="follow twitter">
-		<a href="https://twitter.com/siteorigin" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @siteorigin</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		<iframe allowtransparency="true" frameborder="0" scrolling="no" src="https://platform.twitter.com/widgets/follow_button.html?screen_name=siteorigin&show_count=false&show_screen_name=true" style="width:122px; height:20px;"></iframe>
 	</div>
-	
+
 	<div class="follow facebook">
-		<div class="fb-like" data-href="http://www.facebook.com/SiteOrigin" data-send="false" data-layout="button_count" data-width="120" data-show-faces="true"></div>
+		<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FSiteOrigin&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=222225781217824" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>
 	</div>
 </div>
