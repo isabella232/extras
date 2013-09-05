a9d6688460a09d73aab92c5f42b4a9d496755ad2
Greg Priday <greg@siteorigin.com>
2013-09-05 19:35:01 +0000

    Changed admin bar message.

--- adminbar/adminbar.php
+++ adminbar/adminbar.php
@@ -64,7 +64,7 @@ function siteorigin_adminbar_enqueue( $suffix ) {
 
 	// This adds an extra tab to the theme pages
 	if($suffix == 'theme-install.php' || $suffix == 'themes.php' && !wp_script_is('siteorigin-admin-tab')){
-		wp_enqueue_script('siteorigin-admin-tab', get_template_directory_uri().'/extras/adminbar/assets/tab.js', array('jquery'), SITEORIGIN_THEME_VERSION);
+		wp_enqueue_script('siteorigin-admin-tab', get_template_directory_uri().'/extras/adminbar/assets/tab.js', array( 'jquery' ), SITEORIGIN_THEME_VERSION);
 		wp_localize_script('siteorigin-admin-tab', 'siteoriginAdminTab', array(
 			'text' => __('SiteOrigin Themes', 'siteorigin'),
 			'url' => admin_url('theme-install.php?tab=search&type=author&s=gpriday')
--- adminbar/messages/message-firstrun.php
+++ adminbar/messages/message-firstrun.php
@@ -1,13 +1,3 @@
-<?php _e("Hi! I'm Greg Priday from SiteOrigin.", 'siteorigin') ?> 
-<?php _e("I hope you enjoy my WordPress theme.", 'siteorigin') ?> 
-<?php _e("Please follow me on Twitter and Facebook for updates.", 'siteorigin') ?>
-
-<div class="social">
-	<div class="follow twitter">
-		<iframe allowtransparency="true" frameborder="0" scrolling="no" src="https://platform.twitter.com/widgets/follow_button.html?screen_name=siteorigin&show_count=false&show_screen_name=true" style="width:122px; height:20px;"></iframe>
-	</div>
-
-	<div class="follow facebook">
-		<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2FSiteOrigin&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=222225781217824" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>
-	</div>
-</div>
+<?php
+$theme = basename( get_template_directory() );
+printf( __("Hi! I'm Greg from SiteOrigin. I hope you enjoy using %s. Sign up to <a href='%s' target='_blank'>our newsletter</a> if you'd like more free themes and exclusive offers.", 'siteorigin'), ucfirst($theme), 'http://eepurl.com/hbvhM');
