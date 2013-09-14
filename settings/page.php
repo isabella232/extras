393cdd08bdfee281a0e47fb664114b823d92819e
Greg Priday <greg@siteorigin.com>
2013-09-14 08:57:04 +0000

    Added feature suggestions feature to settings page.

--- settings/page.php
+++ settings/page.php
@@ -9,7 +9,15 @@
 		<?php settings_fields( 'theme_settings' ); ?>
 		<?php do_settings_sections( 'theme_settings' ) ?>
 
-		<p class="submit"><input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Settings', 'siteorigin'); ?>" /></p>
+		<p class="submit">
+			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Settings', 'siteorigin'); ?>" />
+
+			<?php $suggest = apply_filters('siteorigin_settings_suggest_features_url', false); if(!empty($suggest)) : ?>
+				<a id="siteorigin-theme-feature-suggestions" href="<?php echo esc_url($suggest) ?>" target="_blank">
+					<?php _e( 'Suggest features', 'siteorigin' ) ?>
+				</a>
+			<?php endif; ?>
+		</p>
 		<input type="hidden" id="current-tab-field" name="theme_settings_current_tab" value="<?php echo intval(get_theme_mod('_theme_settings_current_tab', 0)) ?>" />
 	</form>
 </div> 
