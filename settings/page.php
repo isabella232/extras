ad9723ba4f2ed6f9912a2976cf3e3655797587c3
Greg Priday <greg@siteorigin.com>
2013-09-18 11:59:59 +0000

    Removed suggest feature.

--- settings/page.php
+++ settings/page.php
@@ -11,10 +11,6 @@
 
 		<p class="submit">
 			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Settings', 'siteorigin'); ?>" />
-
-			<a id="siteorigin-theme-feature-suggestions" href="http://siteorigin.com/suggest-feature/?item=<?php echo esc_attr( basename( get_template_directory() ) ) ?>" target="_blank">
-				<?php _e( 'Suggest a feature', 'siteorigin' ) ?>
-			</a>
 		</p>
 		<input type="hidden" id="current-tab-field" name="theme_settings_current_tab" value="<?php echo intval(get_theme_mod('_theme_settings_current_tab', 0)) ?>" />
 	</form>
